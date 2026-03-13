f44f2a598b874babdeab683c88b89f17510186a4
Nathin A N <nathinan30115@gmail.com>
2026-03-13 23:33:01 +0530

    Create Valid Anagram.java

---
+++ Valid Anagram.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] c=s.toCharArray();
+        char[] ch=t.toCharArray();
+        if(c.length!=ch.length)
+        {
+            return false;
+        }
+        Arrays.sort(c);
+        Arrays.sort(ch);
+        for(int i=0;i<c.length;i++)
+        {
+           if(c[i]!=ch[i])
+           {
+            return false;
+           }
+        }
+        return true;
+    }
+}
+//https://neetcode.io/problems/is-anagram/question
