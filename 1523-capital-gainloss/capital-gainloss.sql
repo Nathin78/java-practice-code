c2ff4ec84fb8ac28369057abb6c77d13e5322609
Nathin A N <nathinan30115@gmail.com>
2026-07-25 23:51:37 +0530

    Time: 552 ms (37.91%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1523-capital-gainloss/capital-gainloss.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select stock_name, SUM(
+    CASE
+        WHEN operation = 'Sell' THEN price
+        ELSE -price
+    END
+ ) as capital_gain_loss
+from Stocks group by stock_name;
