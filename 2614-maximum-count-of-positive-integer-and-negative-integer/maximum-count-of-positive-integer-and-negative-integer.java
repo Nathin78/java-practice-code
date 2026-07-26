3c521c39f052defbeb67c80e1a8a83839fe0388a
Nathin A N <nathinan30115@gmail.com>
2026-07-26 23:30:17 +0530

    Time: 1 ms (24.76%) | Memory: 47 MB (37.36%) - LeetSync

---
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maximumCount(int[] nums) {
+     int po=0;
+     int ne=0;
+     for(int i=0;i<nums.length;i++)
+     {
+        if(nums[i]>0)
+        {
+            po++;
+        }
+        else if(nums[i]<0)
+        {
+            ne++;
+        }
+     }
+     if(po>ne)
+     {
+        return po;
+     }
+     return ne;   
+    }
+}
