c763bf85eb378ee600b78c7e754c073c128ad203
Nathin A N <nathinan30115@gmail.com>
2026-08-01 22:49:27 +0530

    Time: 1 ms (99.61%) | Memory: 45.7 MB (78.15%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -1,30 +1,32 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
          int arr[]=new int[2];
-        // for(int i=0;i<nums.length;i++)
-        // {
-        //     for(int j=i+1;j<nums.length;j++)
-        //     {
-        //         if(nums[i]==nums[j])
-        //         {
-        //             n[i]=nums[i];
-        //         }
-        //     }
-        // }
-        // Arrays.sort(n);
-        int ind=0;
-        HashMap<Integer,Integer>n=new HashMap<>();
-        for(int i:nums)
+         int ind=0;
+        for(int i=0;i<nums.length;i++)
         {
-            n.put(i,n.getOrDefault(i,0)+1);
-        }
-        for(var entry:n.entrySet())
-        {
-            if(entry.getValue()==2)
+            for(int j=i+1;j<nums.length;j++)
             {
-               arr[ind++]= entry.getKey();
+                if(nums[i]==nums[j])
+                {
+                    arr[ind++]=nums[i];
+                }
             }
         }
         return arr;
+        // // Arrays.sort(n);
+        // int ind=0;
+        // HashMap<Integer,Integer>n=new HashMap<>();
+        // for(int i:nums)
+        // {
+        //     n.put(i,n.getOrDefault(i,0)+1);
+        // }
+        // for(var entry:n.entrySet())
+        // {
+        //     if(entry.getValue()==2)
+        //     {
+        //        arr[ind++]= entry.getKey();
+        //     }
+        // }
+        // return arr;
     }
 }
